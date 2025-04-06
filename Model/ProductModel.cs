59318987623f3e4282b81256f4f3056e4c68d772
HponeMarn <supermanph757@gmail.com>
2025-04-06 16:13:01 +0630

    dd

---
+++ Model/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TestA.Model
+{
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ Repositories/ProductReponsitory.cs
@@ -0,0 +1,23 @@
+﻿using Dapper;
+using TestA.Data;
+using TestA.Model;
+
+namespace TestA.Repositories
+{
+    public class ProductReponsitory
+    {
+        private readonly DapperContext _dapperContext;
+        public ProductReponsitory(DapperContext dapperContext)
+        {
+            _dapperContext = dapperContext;
+        }
+        public async Task<IEnumerable<ProductModel>> GetAllProduct()
+        {
+            var quer = @"SELECT * FROM Products ORDER BY CreatedDate DESC";
+            using(var connection=_dapperContext.CreateConnection())
+            {
+                return await connection.QueryAsync<ProductModel>(quer);
+            }
+        }
+    }
+}
